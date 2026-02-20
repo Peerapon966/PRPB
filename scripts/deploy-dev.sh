f8c2e316ab9906c2f31f9c3ea57b1053a5cee2fb
Peerapon966 <peerapon.boonkaweenapanon@gmail.com>
2026-02-20 23:06:11 +0700

    add deploy-dev script

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -3,8 +3,13 @@ set -eo pipefail
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    -a,--auto-approve)
-      
+    -a,--auto-approve-terraform)
+      AUTO_APPROVE_TF=true
+      shift 1
+      ;;
+    -t,--deploy-terraform)
+      DEPLOY_TF=true
+      shift 1
       ;;
     *)
       ;;
@@ -14,4 +19,20 @@ done
 pushd $(dirname -- ${BASH_SOURCE[0]})
 pushd ../terraform
 
-terraform apply -var-file=tfvars/development.tfvars -auto-approve
+aws sts get-caller-identity --profile dev || echo "Couldn't authenticate AWS profile 'dev'"; exit 1
+
+terraform init -backend-config=backend-dev.hcl
+
+ORIGIN_BUCKET_NAME=$(terraform output -raw s3_origin_bucket_name)
+DISTRIBUTION_ID=$(terraform output -raw distribution_id)
+if [[ ${DEPLOY_TF} == true ]]; then
+  terraform apply -var-file=tfvars/development.tfvars "${AUTO_APPROVE_TF:+-auto-approve}"
+fi
+
+pushd ..
+
+npm run build -- --mode development
+
+aws s3 sync dist/ "$ORIGIN_BUCKET_NAME" --delete --exclude "assets/*"
+aws s3 sync src/assets/ "$ORIGIN_BUCKET_NAME" --delete
+aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" --paths "/*"
