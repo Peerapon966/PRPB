5c771d1f55a372b28052b7bd7ddbd07369a1441b
HeftyChonk <peerapon.boonkaweenapanon@gmail.com>
2026-04-22 23:31:09 +0700

    update deploy workflow

--- .github/actions/deploy/deploy.sh
+++ .github/actions/deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 echo "GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME"
 echo ""
@@ -7,14 +7,14 @@ echo "============================="
 echo "|   Setting up variables    |"
 echo "============================="
 echo ""
-GITHUB_BASE_REF_SHA=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.base.sha')
 if [[ "${GITHUB_EVENT_NAME}" = 'pull_request' ]]; then
+  GITHUB_BASE_REF_SHA=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.base.sha')
   git config user.name "github-actions[bot]"
   git config user.email "github-actions[bot]@users.noreply.github.com"
   git tag -f last-base-ref-sha $GITHUB_BASE_REF_SHA
   git push -f origin last-base-ref-sha
 elif [[ "${GITHUB_EVENT_NAME}" = 'workflow_dispatch' ]]; then
-  GITHUB_BASE_REF_SHA=$(git rev-parse last-base-ref-sha)
+  GITHUB_BASE_REF_SHA=${BASE_REF_SHA:-$(git rev-parse last-base-ref-sha)}
 fi
 echo "GITHUB_BASE_REF_SHA: $GITHUB_BASE_REF_SHA"
 
