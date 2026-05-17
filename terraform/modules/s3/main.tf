daa44a3b2ea1417bf3c05fe02b077cc9633272fc
HeftyChonk <peerapon.boonkaweenapanon@gmail.com>
2026-05-17 22:23:24 +0700

    add vulnerabilities for testing snyk

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -33,8 +33,8 @@ resource "aws_s3_bucket_logging" "bucket_logging" {
 
 resource "aws_s3_bucket_public_access_block" "origin_bucket_public_access_block" {
   bucket                  = aws_s3_bucket.origin_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
+  block_public_acls       = false
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
